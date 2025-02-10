b5b8e1cb42e77cc5f4a8c3cf1ff5b986fb38f8f1
Valency012 <00090123@uca.edu.sv>
2025-02-09 22:29:38 -0600

    Mostrando procesos y menu

--- Final_proyect/tarea1_avance.py
+++ Final_proyect/tarea1_avance.py
@@ -1,9 +1,35 @@
 import psutil
-import subprocess
-import time
+#Funcion para listar los procesos en ejecucion
+def listar_procesos():
+    #Imprimir encabezado de la tabla
+    print(f"{'PID':<10}{'Nombre':<30}{'Uso de CPU (%)':<15}{'Uso de Memoria (%)':<15}")
+    print("-" * 70)
+    #:<15, :<30, :>10.2f son especificadores de formato en python
+    #Iterar sobre los procesos en ejecucion y obtener sus atributos
+    for proceso in psutil.process_iter(attrs=['pid', 'name', 'cpu_percent', 'memory_percent']):
+        info = proceso.info #Obtener informacion del proceso
+        print(f"{info['pid']:<10}{info['name']:<30}{info['cpu_percent']:<15.2f}{info['memory_percent']:<15.2f}")
 
-#Mostrar todos los procesos
-def mostrar_procesos():
-    for proceso in psutil.process_iter(['name', 'pid', 'cmdline']):
-        
+#Funcion para mostrar el menu
+def mostrar_menu(): 
+    while True:
+        print("\n Gestion de  Procesos")
+        print("1. Iniciar un proceso")
+        print("2. Matar un proceso")
+        print("3. Monitorear un proceso")
+        print("4. Salir")
 
+        opcion = input("Seleccione una opcion: ")
+
+        if opcion == "4":
+            print("Saliendo...")
+        #aqui faltaria meter el proceso de cada opcion
+        elif opcion in ["1", "2", "3"]:
+            print(f"Opcion {opcion} seleccionada")
+        else: 
+            print("Opcion no valida, intente de nuevo")
+
+#Punto de entrada principal del programa
+if __name__ == "__main__":
+    listar_procesos()
+    mostrar_menu()
